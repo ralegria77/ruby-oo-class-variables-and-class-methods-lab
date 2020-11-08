2af02592a84af06ae1236ed8fd8eb474a2320318
rebec <ralegria77@gmail.com>
2020-11-08 14:47:43 -0800

    commit tree 89e284f3a8dc1e1004aaa1c54af58537dae2344d

---
+++ lib/song.rb
@@ -0,0 +1,57 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+    
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @@count += 1
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@artists << @artist
+        @@genres << @genre
+    end
+
+    def self.count
+        @@count
+    end
+   
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        hash = {}
+        @@genres.each {|gen| hash[gen]= (hash[gen] || 0) + 1}
+        hash 
+    end
+
+    def self.artist_count
+        hash = {}
+        @@artists.each {|gen| hash[gen]= (hash[gen] || 0) + 1}
+        hash 
+    end
+
+end
+
+# def self.genre_count
+#     hash = {}
+#     @@genres.each do |genre|
+#         hash[genre] ||= 0
+#         hash[genre] += 1
+#     end
+#     hash
+# end
+
+# def self.genre_count
+#     result={}
+#     @@genres.each {|gen| result[gen]= (result[gen] || 0) + 1}
+#     result
+# end
